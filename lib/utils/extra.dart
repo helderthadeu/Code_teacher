782b523fdea806ac43a5e5a5d237e12d5169eeeb
helderthadeu <helderthadeubh@gmail.com>
2024-11-22 22:06:14 -0300

    Error updates

--- lib/utils/extra.dart
+++ lib/utils/extra.dart
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import 'utils.dart';
 
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
@@ -73,79 +73,3 @@ class Mnemonico {
     return this._multiplicador;
   }
 }
-=======
-import 'utils.dart';
-
-import 'package:flutter_blue_plus/flutter_blue_plus.dart';
-
-final Map<DeviceIdentifier, StreamControllerReemit<bool>> _cglobal = {};
-final Map<DeviceIdentifier, StreamControllerReemit<bool>> _dglobal = {};
-
-/// connect & disconnect + update stream
-extension Extra on BluetoothDevice {
-  // convenience
-  StreamControllerReemit<bool> get _cstream {
-    _cglobal[remoteId] ??= StreamControllerReemit(initialValue: false);
-    return _cglobal[remoteId]!;
-  }
-
-  // convenience
-  StreamControllerReemit<bool> get _dstream {
-    _dglobal[remoteId] ??= StreamControllerReemit(initialValue: false);
-    return _dglobal[remoteId]!;
-  }
-
-  // get stream
-  Stream<bool> get isConnecting {
-    return _cstream.stream;
-  }
-
-  // get stream
-  Stream<bool> get isDisconnecting {
-    return _dstream.stream;
-  }
-
-  // connect & update stream
-  Future<void> connectAndUpdateStream() async {
-    _cstream.add(true);
-    try {
-      await connect(mtu: null);
-    } finally {
-      _cstream.add(false);
-    }
-  }
-
-  // disconnect & update stream
-  Future<void> disconnectAndUpdateStream({bool queue = true}) async {
-    _dstream.add(true);
-    try {
-      await disconnect(queue: queue);
-    } finally {
-      _dstream.add(false);
-    }
-  }
-}
-
-class Mnemonico {
-  String _comando = "nada";
-  String _multiplicador = "nada";
-
-  Mnemonico(this._comando, this._multiplicador);
-
-  void setComando(String comando) {
-    this._comando = comando;
-  }
-
-  void setMultiplicador(String multiplicador) {
-    this._multiplicador = multiplicador;
-  }
-
-  String getComando() {
-    return this._comando;
-  }
-
-  String getMultiplicador() {
-    return this._multiplicador;
-  }
-}
->>>>>>> 2c51d7e706cdd9c7d057bcef8ef1e7503d1625c5
